ea559b28d643aa55a3639a71eff22db20a78b207
Brendaibarra28 <104107587+Brendaibarra28@users.noreply.github.com>
2022-04-27 17:18:43 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,12 @@
+console.log("cocinar una torta");
+
+console.log("2 huevos");
+console.log("10 cdas de azucar");
+console.log("esencia de vainilla");
+console.log("10 cdas de aceite");
+console.log("10 cdas de leche");
+console.log("10 cdas de harina leudante");
+console.log("dulce de leche 300 gr.");
+console.log("mous de chocolate en polvo");
+console.log("leche c/n");
+console.log("azucar c/n");
